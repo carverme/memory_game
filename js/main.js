14e41c644236e4332f2b4e36615b87c74e5d620d
Matt Carver <carverme@Matts-MacBook-Pro.local>
2018-05-19 18:57:09 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,21 +1,30 @@
 console.log("Up and running!");
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-
-var cardOne = cards[0];
-cardsInPlay.push('cardOne');
-console.log("User flipped queen" + " " + cardsInPlay['cardOne']);
-
-var cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped king" + " " + cardsInPlay['cardTwo']);
-
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+}
+var flipCard = function (cardId) {
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
 if (cardsInPlay === 2) {	
 } else if (cardsInPlay[0] === cardsInPlay[1]) {	
 	alert("You found a match!");
 } else if (cardsInPlay[0] !== cardsInPlay[1]) {
 	alert("Sorry, try again.");
 }
+checkForMatch();	
+}
+	flipCard(0);
+	flipCard(2);
+
+
+
+
 
 /*alert('Hello!');
 console.log("User flipped" + cardOne);
