54b936adacbcad4daf09a8f38354896aab1a4584
Matt Carver <carverme@Matts-MacBook-Pro.local>
2018-05-20 00:46:42 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 console.log("Up and running!");
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -9,8 +30,10 @@ console.log("Sorry, try again.");
 }
 }
 var flipCard = function (cardId) {
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+console.log("User flipped " + cards[cardId].suit);
+console.log(cards[cardId].cardImage);
+cardsInPlay.push(cards[cardId].rank);
 if (cardsInPlay === 2) {	
 } else if (cardsInPlay[0] === cardsInPlay[1]) {	
 	alert("You found a match!");
@@ -21,12 +44,3 @@ checkForMatch();
 }
 	flipCard(0);
 	flipCard(2);
-
-
-
-
-
-/*alert('Hello!');
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardTwo);
-*/
